18a41ad7cee98f59434d5ea9477ff3e8516ec3b6
GuilhermeDeOliveiraAmorim <guilherme.o.a.ufal@gmail.com>
2022-11-06 13:59:12 -0300

    feat: added zod

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors";
 import { PrismaClient } from "@prisma/client";
+import { z } from "zod";
 
 const prisma = new PrismaClient({
     log: ["query"],
@@ -18,6 +19,15 @@ async function bootstrap() {
     fastify.get("/pools/count", async () => {
         const count = await prisma.pool.count();
         return { count };
+    });
+
+    fastify.post("/pools", async (request, reply) => {
+        const createPoolBody = z.object({
+            title: z.string(),
+        });
+
+        const { title } = createPoolBody.parse(request.body);
+        return reply.status(201).send({ title });
     });
 
     await fastify.listen({ port: 3333, host: "0.0.0.0" });
